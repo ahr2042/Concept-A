85979ef4df73a40746714b9446181ff2fe6d3456
ahr2042 <rashed-a@web.de>
2024-11-03 12:32:22 +0100

    - create a skeleton GUI with intial funcitons

--- concept_A/gui/gui.cpp
+++ concept_A/gui/gui.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "gui.h"
 
+
+
 gui::gui(QWidget *parent)
     : QMainWindow(parent)
 {
@@ -9,3 +11,93 @@ gui::gui(QWidget *parent)
 
 gui::~gui()
 {}
+
+e_ErrorState gui::f_e_updateDevicesCombobox()
+{
+    ui.m_cmbBx_devicesList->clear();
+    char* v_pc_devicesNames = new char[MAX_PARAMETER_SIZE];
+    API_f_v_getDevicesNames(v_pc_devicesNames);
+    QString v_o_deviesNames(v_pc_devicesNames);
+    
+    delete[] v_pc_devicesNames;
+    v_pc_devicesNames = nullptr;
+
+    if (v_o_deviesNames.isEmpty())
+    {
+        return MF_NO_VIDEO_DEVICE_ERR;
+    }
+    QStringList v_o_namesList = v_o_deviesNames.split(";");
+    ui.m_cmbBx_devicesList->addItems(v_o_namesList);       
+    for (QString v_o_device : v_o_namesList)
+    {        
+        f_v_updateLogWithNewInfo(v_o_device);
+    }            
+}
+
+int gui::f_i_getDeviceIdFromCombobox()
+{    
+    m_i_deviceId = ui.m_cmbBx_devicesList->currentIndex();
+    return m_i_deviceId;
+}
+
+e_ErrorState gui::f_e_startStreaming(int i_i_deviceId)
+{
+    e_ErrorState v_e_errorState = NO_ERR;
+    v_e_errorState = (e_ErrorState)API_f_i_setDevice(m_i_deviceId);
+    if (v_e_errorState != NO_ERR)
+    {
+        // update log error
+        return v_e_errorState;
+    }
+    v_e_errorState = (e_ErrorState)API_f_i_startStreaming();
+    return v_e_errorState;
+}
+
+void gui::f_v_updateLogWithNewInfo(QString i_o_newInfo)
+{
+    QString v_o_Log = ui.m_lbl_log->text() + i_o_newInfo + "\n";
+    ui.m_lbl_log->setText(v_o_Log);
+}
+
+QString gui::f_o_interpreteError(e_ErrorState i_e_errorId)
+{
+    char* v_pc_devicesNames = new char[MAX_PARAMETER_SIZE];
+    API_f_v_interpreteError((int)i_e_errorId, v_pc_devicesNames);
+    QString v_o_deviesNames(v_pc_devicesNames);
+
+    delete[] v_pc_devicesNames;
+    v_pc_devicesNames = nullptr;
+
+    return v_o_deviesNames;
+}
+
+
+void gui::f_v_handleButtonPress()
+{
+    QString v_o_buttonName = qobject_cast<QPushButton*>(sender())->objectName();    
+    e_ErrorState v_e_errorState = NO_ERR;
+    if (v_o_buttonName == ui.m_btn_init->objectName())
+    {        
+        API_f_v_create();
+        v_e_errorState = (e_ErrorState)API_f_i_init();
+        if (v_e_errorState == NO_ERR)
+        {            
+            v_e_errorState = f_e_updateDevicesCombobox();
+        }
+    }
+    else if (v_o_buttonName == ui.m_btn_start->objectName())
+    {
+
+    }
+    else if (v_o_buttonName == ui.m_btn_stop->objectName())
+    {
+
+    }
+
+    if (v_e_errorState != NO_ERR)
+    {
+        f_v_updateLogWithNewInfo(f_o_interpreteError(v_e_errorState));
+    }
+}
+
+
--- concept_A/gui/gui.h
+++ concept_A/gui/gui.h
@@ -3,6 +3,11 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_gui.h"
 
+#include "e_ErrorState.h"
+#include "getterLib.h"
+
+
+
 class gui : public QMainWindow
 {
     Q_OBJECT
@@ -11,6 +16,16 @@ public:
     gui(QWidget *parent = nullptr);
     ~gui();
 
+private slots:
+    void f_v_handleButtonPress();
+
 private:
     Ui::guiClass ui;
+    int m_i_deviceId = -1;
+
+    e_ErrorState f_e_updateDevicesCombobox();
+    int f_i_getDeviceIdFromCombobox();
+    e_ErrorState f_e_startStreaming(int i_i_deviceId);
+    void f_v_updateLogWithNewInfo(QString i_o_newInfo);
+    QString f_o_interpreteError(e_ErrorState i_e_errorId);
 };
