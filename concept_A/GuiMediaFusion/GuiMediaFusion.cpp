fd07f70fed415adfdd0d12955ca02bf93ee348bb
ahr2042 <rashed-a@web.de>
2024-12-15 13:26:13 +0100

    - Gui project, no implementation (testing)

---
+++ concept_A/GuiMediaFusion/GuiMediaFusion.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "GuiMediaFusion.h"
+
+GuiMediaFusion::GuiMediaFusion(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
+
+GuiMediaFusion::~GuiMediaFusion()
+{}
---
+++ concept_A/GuiMediaFusion/main.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "GuiMediaFusion.h"
+#include <QtWidgets/QApplication>
+#include "MediaFusionGCV_API.h"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    mediaLib_GStreamerInit(argc, argv);
+    int32_t objectID = mediaLib_create(Camera);
+    deviceProperties* foundDevices = nullptr;
+    int32_t numberOfDevices = 0;
+    int32_t result = mediaLib_getDevices(objectID, numberOfDevices, &foundDevices);
+    if (result == 0 && foundDevices != nullptr)
+    {
+        for (int i = 0; i < numberOfDevices; i++)
+        {
+            std::cout << "Device number " << i + 1 << ":" << std::endl;
+            std::cout << foundDevices[i].deviceName << std::endl << foundDevices[i].formattedDeviceCapabilities << std::endl;
+        }
+    }
+    
+    return 0;
+    //QApplication a(argc, argv);
+    //GuiMediaFusion w;
+    //w.show();
+    //return a.exec();
+}
