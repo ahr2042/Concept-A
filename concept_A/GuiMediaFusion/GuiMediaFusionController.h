4bdf16f94e97b172f35ed6cb0965bce0fdf9dea8
ahr2042 <rashed-a@web.de>
2025-01-19 12:35:42 +0100

    - implement needed init funcitons in controller class - adjust gui errors enum

--- concept_A/GuiMediaFusion/GuiMediaFusionController.h
+++ concept_A/GuiMediaFusion/GuiMediaFusionController.h
@@ -1,18 +1,24 @@
 #pragma once
 
 #include "GuiMediaFusion.h"
+#include "GuiMediaFusionModel.h"
 
 class GuiMediaFusionController : public QObject
 {
 	Q_OBJECT
 public:
 	GuiMediaFusionController();
+	~GuiMediaFusionController();
 
 public slots:
 	void handleViewRequest(QString);
 
 
 private:
-	GuiMediaFusion* guiMediaFusionView = nullptr;
+	GuiMediaFusion* MF_View = nullptr;
+	GuiMediaFusionModel* MF_Model = nullptr;
+
+	errorStateGui connectToView();
+	errorStateGui connectToModel();
 
 };
