a4750091e68224948fd76cebbcb4dec1f84f5506
ahr2042 <rashed-a@web.de>
2025-02-04 23:01:11 +0100

    - create pipe line initial logic

--- concept_A/GuiMediaFusion/GuiMediaFusionModel.cpp
+++ concept_A/GuiMediaFusion/GuiMediaFusionModel.cpp
@@ -18,3 +18,42 @@ errorStateGui GuiMediaFusionModel::initGstreamer(int argc, char** argv)
 	return errorStateGui::NO_ERR;
 }
 
+size_t GuiMediaFusionModel::createPipeline()
+{	
+	pipelineStash.push_back(new pipeLine);
+
+	size_t stashSize = pipelineStash.size();
+	size_t piplineID = mediaLib_create();
+	pipelineStash[stashSize]->pipelineId = piplineID;
+
+	
+	return size_t();
+}
+
+errorStateGui GuiMediaFusionModel::getAvailableDevices(size_t piplineID)
+{
+	size_t devicesCount = 0;
+	deviceProperties* devices = nullptr;
+	if (mediaLib_getDevices(piplineID, devicesCount, &devices) == errorState::NO_ERR)
+	{
+		if (devices != nullptr)
+		{
+			for (int i = 0; i < devicesCount; i++)
+			{ create the the correct format to send it in a signal to the view class via control class
+				std::cout << "Device number " << i + 1 << ":" << std::endl;
+				std::cout << foundDevices[i].deviceName << std::endl << foundDevices[i].formattedDeviceCapabilities << std::endl;
+			}
+		}
+	}
+	return errorStateGui::GET_AVAILABLE_DEVICES_ERR;
+}
+errorStateGui GuiMediaFusionModel::updatePipelineInfo(QString incomingInformation)
+{
+	incomingInformation.trimmed();
+	//if (!incomingInformation.isEmpty())
+	//{
+	//	QStringList  incomingInformation.split(";", Qt::SkipEmptyParts);
+
+	//}
+	return errorStateGui::NO_ERR;	
+}
--- concept_A/GuiMediaFusion/GuiMediaFusionModel.h
+++ concept_A/GuiMediaFusion/GuiMediaFusionModel.h
@@ -2,6 +2,7 @@
 #include "MediaFusionGCV_API.h"
 #include <qobject.h>
 #include "errorStateGui.h"
+#include <vector>
 class GuiMediaFusionModel : public QObject
 {
 	Q_OBJECT
@@ -9,7 +10,18 @@ public:
 	GuiMediaFusionModel();
 	~GuiMediaFusionModel();
 
-	errorStateGui initGstreamer(int, char**);	
+	errorStateGui initGstreamer(int, char**);
+	size_t createPipeline();
+
+public slots:	
+	errorStateGui updatePipelineInfo(QString);
+signals:	
+	void updateSourceDeviceInfo(deviceProperties);
+	void updateSinkDeviceInfo(deviceProperties);
 
 private:
+	std::vector<pipeLine*> pipelineStash;
+	errorStateGui getAvailableDevices(size_t);
+	
+	
 };
